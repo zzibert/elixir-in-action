317855a4a206febc8d7a5120d5d1606fb0afdf36
Žan Žiber <zan.zibert@ulu.io>
2020-03-13 13:59:46 +0100

    new mix project e_i_a

---
+++ e_i_a/lib/eia.ex
@@ -0,0 +1,18 @@
+defmodule EIA do
+  @moduledoc """
+  Documentation for EIA.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EIA.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ e_i_a/test/eia_test.exs
@@ -0,0 +1,8 @@
+defmodule EIATest do
+  use ExUnit.Case
+  doctest EIA
+
+  test "greets the world" do
+    assert EIA.hello() == :world
+  end
+end
