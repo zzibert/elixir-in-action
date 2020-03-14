cac67a8942e4b958a8249577483cdfd81805042e
zan <zan_zibert@hotmail.com>
2020-03-14 13:22:09 +0100

    functions as first class citizens

--- e_i_a/lib/eia.ex
+++ e_i_a/lib/eia.ex
@@ -1,11 +1,4 @@
 defmodule EIA do
-  def area(a), do: area(a, a)
+  square = fn x -> x * x end
 
-  def area(a, b), do: a * b
-
-  def sum(a), do: sum(a, 0)
-
-  defp sum(a, b), do: a + b
-
-  def double(a), do: sum(a, a)
 end
