fcbabd04dbad4579c620149cf1de8d56253aa672
zan <zan_zibert@hotmail.com>
2020-03-14 19:06:15 +0100

    when matching a map, the left-side pattern doesnt need to contain all the keys from the right-side term

--- e_i_a/lib/eia.ex
+++ e_i_a/lib/eia.ex
@@ -1,5 +1,3 @@
 defmodule EIA do
-  days = MapSet.new([:monday, :tuesday, :wednesday])
-  days = MapSet.put(days, :thursday)
-  Enum.each(days, &IO.puts/1)
+  %{age: age} = %{name: "Bob", age: 25}
 end
