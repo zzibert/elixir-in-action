1d1aa56265c81b38a7a6e9c8393670c57ae13daa
zan <zan_zibert@hotmail.com>
2020-03-16 20:47:22 +0100

    Querying the to-do list

--- e_i_a/lib/todo.ex
+++ e_i_a/lib/todo.ex
@@ -4,4 +4,8 @@ defmodule TodoList do
   def add_entry(todo_list, date, title) do
     Map.update(todo_list, date, [title], fn titles -> [title | titles] end)
   end
+
+  def entries(todo_list, date) do
+    Map.get(todo_list, date, [])
+  end
 end
