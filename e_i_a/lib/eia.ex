054d0073d6641906d1f1272b3f03a14f92fc46b7
Žan Žiber <zan.zibert@ulu.io>
2020-03-16 15:12:43 +0100

    calculating the sum of the list

--- e_i_a/lib/eia.ex
+++ e_i_a/lib/eia.ex
@@ -1,7 +1,6 @@
 defmodule EIA do
-  def print(1), do: IO.puts(1)
-  def print(n) do
-    print(n - 1)
-    IO.puts(n)
+  def sum([]), do: 0
+  def sum([head | tail]) do
+    head + sum(tail)
   end
 end
