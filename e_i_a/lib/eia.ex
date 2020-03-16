88d7cd4bbaea80304d804b72d337428dfbfca38d
Žan Žiber <zan.zibert@ulu.io>
2020-03-16 15:22:45 +0100

    tail-recursive sum of the first n natural numbers

--- e_i_a/lib/eia.ex
+++ e_i_a/lib/eia.ex
@@ -1,6 +1,14 @@
 defmodule EIA do
-  def sum([]), do: 0
-  def sum([head | tail]) do
-    head + sum(tail)
+  def sum(list) do
+    do_sum(0, list)
+  end
+
+  defp do_sum(current_sum, []) do
+    current_sum
+  end
+
+  defp do_sum(current_sum, [head | tail]) do
+    new_sum = head + current_sum
+    do_sum(new_sum, tail)
   end
 end
