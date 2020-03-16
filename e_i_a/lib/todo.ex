bae40b6e111edbdb9dd568b53295574ffecc080a
zan <zan_zibert@hotmail.com>
2020-03-16 21:01:23 +0100

    TodoList relying on MultiDict

--- e_i_a/lib/todo.ex
+++ e_i_a/lib/todo.ex
@@ -1,11 +1,12 @@
 defmodule TodoList do
-  def new(), do: %{}
+  require MultiDict
+  def new(), do: MultiDict.new()
 
   def add_entry(todo_list, date, title) do
-    Map.update(todo_list, date, [title], fn titles -> [title | titles] end)
+    MultiDict.add(todo_list, date)
   end
 
   def entries(todo_list, date) do
-    Map.get(todo_list, date, [])
+    MultiDict.get(todo_list, date)
   end
 end
