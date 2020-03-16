f6cf402a1d294ba5c4477d2a3cc2c36def7fdc15
Žan Žiber <zan.zibert@ulu.io>
2020-03-16 12:36:41 +0100

    with-based user extraction

--- e_i_a/lib/eia.ex
+++ e_i_a/lib/eia.ex
@@ -1,3 +1,9 @@
 defmodule EIA do
-  %{age: age} = %{name: "Bob", age: 25}
+  def extract_user(user) do
+    with {:ok, login} <- extract_login(user),
+         {:ok, email} <- extract_email(user),
+         {:ok, password} <- extract_password(user) do
+      {:ok, %{login: login, email: email, password: password}}       
+    end
+  end
 end
