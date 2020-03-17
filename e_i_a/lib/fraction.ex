1c6adb57b393083783d79c76898d9e4e31b851f4
Žan Žiber <zan.zibert@ulu.io>
2020-03-17 14:14:55 +0100

    adding two fractions

--- e_i_a/lib/fraction.ex
+++ e_i_a/lib/fraction.ex
@@ -8,4 +8,8 @@ defmodule Fraction do
   def value(%Fraction{a: a, b: b}) do
     a / b
   end
+
+  def add(%Fraction{a: a1, b: b1}, %Fraction{a: a2, b: b2}) do
+    new(a1 * b2 + a2 * b1, b1 * b2)
+  end
 end
