bd456a573b87f65f611d13b1e97ec7a98761c0ae
Žan Žiber <zan.zibert@ulu.io>
2020-03-17 15:57:52 +0100

    updating an entry

--- e_i_a/lib/todo_crud.ex
+++ e_i_a/lib/todo_crud.ex
@@ -21,4 +21,16 @@ defmodule TodoList do
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
     |> Enum.map(fn {_, entry} -> entry end)
   end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
 end
