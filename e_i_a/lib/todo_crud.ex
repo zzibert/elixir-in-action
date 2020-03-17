3dcf34ba8e93747c9d28c9336fb0334a9f958509
Žan Žiber <zan.zibert@ulu.io>
2020-03-17 15:46:49 +0100

    filtering entries for a given date

--- e_i_a/lib/todo_crud.ex
+++ e_i_a/lib/todo_crud.ex
@@ -15,4 +15,10 @@ defmodule TodoList do
 
     %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
   end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
 end
