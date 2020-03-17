d683d5565b80c8146779a7a4bf673e392dbfabb7
Žan Žiber <zan.zibert@ulu.io>
2020-03-17 16:11:03 +0100

    second version of update_entry

--- e_i_a/lib/todo_crud.ex
+++ e_i_a/lib/todo_crud.ex
@@ -22,13 +22,18 @@ defmodule TodoList do
     |> Enum.map(fn {_, entry} -> entry end)
   end
 
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
   def update_entry(todo_list, entry_id, updater_fun) do
     case Map.fetch(todo_list.entries, entry_id) do
       :error ->
         todo_list
 
       {:ok, old_entry} ->
-        new_entry = updater_fun.(old_entry)
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
         new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
         %TodoList{todo_list | entries: new_entries}
     end
