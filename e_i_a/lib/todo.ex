47d7f77fb64fbe44b8b7736f1a79cc6edd1be376
zan <zan_zibert@hotmail.com>
2020-03-18 21:26:37 +0100

    delete entry

--- e_i_a/lib/todo.ex
+++ e_i_a/lib/todo.ex
@@ -1,12 +1,12 @@
-defmodule TodoList do
-  require MultiDict
-  def new(), do: MultiDict.new()
+# defmodule TodoList do
+#   require MultiDict
+#   def new(), do: MultiDict.new()
 
-  def add_entry(todo_list, entry) do
-    MultiDict.add(todo_list, entry.date, entry)
-  end
+#   def add_entry(todo_list, entry) do
+#     MultiDict.add(todo_list, entry.date, entry)
+#   end
 
-  def entries(todo_list, date) do
-    MultiDict.get(todo_list, date)
-  end
-end
+#   def entries(todo_list, date) do
+#     MultiDict.get(todo_list, date)
+#   end
+# end
