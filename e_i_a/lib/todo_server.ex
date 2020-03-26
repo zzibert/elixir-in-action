dfbccf58d6f72b750b7cbc22f7f038e99f6aa129
zan <zan_zibert@hotmail.com>
2020-03-26 19:49:27 +0100

    TodoServer basic structure

--- e_i_a/lib/todo_server.ex
+++ e_i_a/lib/todo_server.ex
@@ -1,5 +1,15 @@
 defmodule TodoServer do
+  def start do
+    spawn(fn -> loop(TodoList.new()) end)
+  end
 
+  defp loop(todo_list) do
+    new_todo_list =
+      receive do
+        message -> process_message(todo_list, message)
+      end
+    loop(new_todo_list)
+  end
 end
 
 defmodule TodoList do
