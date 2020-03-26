7ab19545e5d36e5fa6eba77d3539e24bfb8f2bc7
zan <zan_zibert@hotmail.com>
2020-03-26 19:19:58 +0100

    concurrent stateful calculator

---
+++ e_i_a/lib/calculator.ex
@@ -0,0 +1,19 @@
+defmodule Calculator do
+  defp loop(current_value) do
+    new_value =
+      receive do
+        {:value, caller} ->
+          send(caller, {:response, current_value})
+          current_value
+        {:add, value} -> current_value + value
+        {:sub, value} -> current_value - value
+        {:mul, value} -> current_value * value
+        {:div, value} -> current_value / value
+
+        invalid_request ->
+          IO.puts("Invalid request: #{invalid_request}")
+          current_value
+      end
+    loop(new_value)
+  end
+end
