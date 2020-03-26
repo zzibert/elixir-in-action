0c04eef560892a355eb03f0d93696394097b7789
zan <zan_zibert@hotmail.com>
2020-03-26 18:18:19 +0100

    database server loop

--- e_i_a/lib/database_server.ex
+++ e_i_a/lib/database_server.ex
@@ -5,8 +5,14 @@ defmodule DatabaseServer do
 
   defp loop do
     receive do
-      ...
+      {:run_query, caller, query_def} ->
+        send(caller, {:query_result, run_query(query_def)})
     end
     loop()
+  end
+
+  defp run_query(query_def) do
+    Process.sleep(2000)
+    "#{query_def} result"
   end
 end
