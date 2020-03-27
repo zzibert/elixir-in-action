736e88bacdba57b4f686debbd12d9bc16b9ee581
Žan Žiber <zan.zibert@ulu.io>
2020-03-27 11:20:30 +0100

    key/value store implementation

--- e_i_a/lib/server_process.ex
+++ e_i_a/lib/server_process.ex
@@ -29,3 +29,17 @@ defmodule ServerProcess do
     end
   end
 end
+
+defmodule KeyValueStore do
+  def init() do
+    %{}
+  end
+
+  def handle_call({:put, key, value}, state) do
+    {:ok, Map.put(state, key, value)}
+  end
+
+  def handle_call({:get, key}, state) do
+    {Map.get(state, key), state}
+  end
+end
