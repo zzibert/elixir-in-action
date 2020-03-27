6835e41e67a9535eb114f79d5eeba151fb55a570
zan <zan_zibert@hotmail.com>
2020-03-27 21:17:34 +0100

    adding interface functions

--- e_i_a/lib/key_value_gen_server.ex
+++ e_i_a/lib/key_value_gen_server.ex
@@ -4,10 +4,18 @@ defmodule KeyValueStore do
   # CLIENT
 
   def start() do
-
+    GenServer.start(KeyValueStore, nil)
   end
 
-  def init() do
+  def put(pid, key, value) do
+    GenServer.cast(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+
+  def init(_) do
     {:ok, %{}}
   end
 
@@ -15,7 +23,7 @@ defmodule KeyValueStore do
     {:noreply, Map.put(state, key, value)}
   end
 
-  def handle_call({:get, key}, state) do
+  def handle_call({:get, key}, _from, state) do
     {:reply, Map.get(state, key), state}
   end
 end
