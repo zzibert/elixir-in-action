6d26a94c92237ee2a85eb9e2f16628e16a420f11
Žan Žiber <zan.zibert@ulu.io>
2020-03-27 11:25:54 +0100

    wrapping ServerProcess function calls

--- e_i_a/lib/server_process.ex
+++ e_i_a/lib/server_process.ex
@@ -35,6 +35,18 @@ defmodule KeyValueStore do
     %{}
   end
 
+  def start do
+    ServerProcess.start(KeyValueStore)
+  end
+
+  def put(pid, key, value) do
+    ServerProcess.call(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    ServerProcess.call(pid, {:get, key})
+  end
+
   def handle_call({:put, key, value}, state) do
     {:ok, Map.put(state, key, value)}
   end
