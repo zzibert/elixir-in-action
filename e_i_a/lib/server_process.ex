6a9eb303d4669707d40bc526afb5f257da4eb74e
zan <zan_zibert@hotmail.com>
2020-03-27 20:44:57 +0100

    refactoring todo server

--- e_i_a/lib/server_process.ex
+++ e_i_a/lib/server_process.ex
@@ -1,46 +1,46 @@
-defmodule ServerProcess do
-  def start(callback_module) do
-    spawn(fn ->
-      initial_state = callback_module.init()
-      loop(callback_module, initial_state)
-    end)
-  end
+# defmodule ServerProcess do
+#   def start(callback_module) do
+#     spawn(fn ->
+#       initial_state = callback_module.init()
+#       loop(callback_module, initial_state)
+#     end)
+#   end
 
-  def cast(server_pid, request) do
-    send(server_pid, {:cast, request})
-  end
+#   def cast(server_pid, request) do
+#     send(server_pid, {:cast, request})
+#   end
 
-  def call(server_pid, request) do
-    send(server_pid, {:call, request, self()})
+#   def call(server_pid, request) do
+#     send(server_pid, {:call, request, self()})
 
-    receive do
-      {:response, response} ->
-        response
-    end
-  end
+#     receive do
+#       {:response, response} ->
+#         response
+#     end
+#   end
 
-  defp loop(callback_module, current_state) do
-    receive do
-      {:call, request, caller} ->
-        {response, new_state} =
-          callback_module.handle_call(
-            request,
-            current_state
-          )
+#   defp loop(callback_module, current_state) do
+#     receive do
+#       {:call, request, caller} ->
+#         {response, new_state} =
+#           callback_module.handle_call(
+#             request,
+#             current_state
+#           )
 
-        send(caller, {:response, response})
+#         send(caller, {:response, response})
 
-      {:cast, request} ->
-        new_state =
-          callback_module.handle_cast(
-            request,
-            current_state
-          )
+#       {:cast, request} ->
+#         new_state =
+#           callback_module.handle_cast(
+#             request,
+#             current_state
+#           )
 
-        loop(callback_module, new_state)
-    end
-  end
-end
+#         loop(callback_module, new_state)
+#     end
+#   end
+# end
 
 defmodule KeyValueStore do
   def init() do
@@ -61,10 +61,6 @@ defmodule KeyValueStore do
 
   def handle_cast({:put, key, value}, state) do
     Map.put(state, key, value)
-  end
-
-  def handle_call({:put, key, value}, state) do
-    {:ok, Map.put(state, key, value)}
   end
 
   def handle_call({:get, key}, state) do
