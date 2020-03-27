e0ab4f215bd426045522d5477e0325e69812e005
Žan Žiber <zan.zibert@ulu.io>
2020-03-27 11:10:28 +0100

    handling messages in the server process

--- e_i_a/lib/server_process.ex
+++ e_i_a/lib/server_process.ex
@@ -5,4 +5,18 @@ defmodule ServerProcess do
       loop(callback_module, initial_state)
     end)
   end
+
+  defp loop(callback_module, current_state) do
+    receive do
+      {request, caller} ->
+        {response, new_state} =
+          callback_module.handle_call(
+            request,
+            current_state
+          )
+
+        send(caller, {:response, response})
+        loop(callback_module, new_state)
+    end
+  end
 end
