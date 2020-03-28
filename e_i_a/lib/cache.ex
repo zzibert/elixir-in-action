724e0c15bce77b49da4de29d400741ec77562601
zan <zan_zibert@hotmail.com>
2020-03-28 13:34:29 +0100

    handling the server process request

--- e_i_a/lib/cache.ex
+++ e_i_a/lib/cache.ex
@@ -4,4 +4,15 @@ defmodule Todo.Cache do
   def init(_) do
     {:ok, %{}}
   end
+
+  def handle_call({:server_process, todo_list_name}, _, todo_servers) do
+    case Map.fetch(todo_servers, todo_list_name) do
+      {:ok, todo_server} ->
+        {:reply, todo_server, todo_servers}
+
+      :error ->
+        {:ok, new_server} = Todo.Server.start()
+        {:reply, new_server, Map.put(todo_servers, todo_list_name, new_server)}
+    end
+  end
 end
