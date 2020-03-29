1e4bd95134b9b4376aaf10c4cbab22dbf4cceda3
zan <zan_zibert@hotmail.com>
2020-03-29 20:43:23 +0200

    persisting the data

--- todo/lib/server.ex
+++ todo/lib/server.ex
@@ -21,12 +21,19 @@ defmodule Todo.Server do
 
   @impl GenServer
   def init(name) do
-    {:ok, %{list: List.new(), name: name}}
+    state =
+      case Todo.Database.get(name) do
+        content -> content
+        _ -> List.new()
+      end
+    {:ok, %{list: state, name: name}}
   end
 
   @impl GenServer
-  def handle_cast({:add_entry, new_entry}, %{list: list} = state) do
-    {:noreply, %{state | list: List.add_entry(list, new_entry)}}
+  def handle_cast({:add_entry, new_entry}, %{list: list, name: name} = state) do
+    new_list = List.add_entry(list, new_entry)
+    Todo.Database.store(name, new_list)
+    {:noreply, %{state | list: new_list}}
   end
 
   @impl GenServer
