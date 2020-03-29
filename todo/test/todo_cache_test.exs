0489ff436172e2e1576b7c96a66a116fbb73080f
zan <zan_zibert@hotmail.com>
2020-03-29 18:35:19 +0200

    testing the to-do operations

--- todo/test/todo_cache_test.exs
+++ todo/test/todo_cache_test.exs
@@ -10,4 +10,13 @@ defmodule Todo.Cache.Test do
     assert bob_pid != Cache.server_process(cache, "alice")
     assert bob_pid == Cache.server_process(cache, "bob")
   end
+
+  test "to-do operations" do
+    {:ok, cache} = Todo.Cache.start()
+    alice = Todo.Cache.server_process(cache, "alice")
+    Todo.Server.add_entry(alice, %{date: ~D[2018-12-19], title: "Dentist"})
+    entries = Todo.Server.entries(alice, ~D[2018-12-19])
+
+    assert [%{date: ~D[2018-12-19], title: "Dentist"}] = entries
+  end
 end
