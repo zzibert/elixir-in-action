60bdc800fb0f652e05fbc9258760c9b8ec29449a
zan <zan_zibert@hotmail.com>
2020-04-08 20:33:11 +0200

    finding a to-do server

--- todo/lib/cache.ex
+++ todo/lib/cache.ex
@@ -1,34 +1,34 @@
 defmodule Todo.Cache do
   use DynamicSupervisor
-  # INTERFACE #
 
   def start_link(_) do
     IO.puts("Starting to-do cache.")
+
     DynamicSupervisor.start_link(
       name: __MODULE__,
       strategy: :one_for_one
     )
   end
 
+  def child_spec(_arg) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, []},
+      type: :supervisor
+    }
+  end
+
   def server_process(todo_list_name) do
-    GenServer.call(__MODULE__, {:server_process, todo_list_name})
-  end
-
-  # CALLBACKS #
-
-  def init(_) do
-    {:ok, %{}}
-  end
-
-  def handle_call({:server_process, todo_list_name}, _, todo_servers) do
-    case Map.fetch(todo_servers, todo_list_name) do
-      {:ok, todo_server} ->
-        {:reply, todo_server, todo_servers}
-
-      :error ->
-        IO.puts("Starting to-do server for #{todo_list_name}")
-        {:ok, new_server} = Todo.Server.start_link(todo_list_name)
-        {:reply, new_server, Map.put(todo_servers, todo_list_name, new_server)}
+    case start_child(todo_list_name) do
+      {:ok, pid} -> pid
+      {:error, {:already_started, pid}} -> pid
     end
+  end
+
+  defp start_child(todo_list_name) do
+    DynamicSupervisor.start_child(
+      __MODULE__,
+      {Todo.Server, todo_list_name}
+    )
   end
 end
