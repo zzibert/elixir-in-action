fd5fcfead80c751c32d2216755ff305f96191e5c
zan <zan_zibert@hotmail.com>
2020-04-22 20:36:53 +0200

    http server specification

---
+++ todo/lib/web.ex
@@ -0,0 +1,10 @@
+defmodule Todo.Web do
+
+  def child_spec(_arg) do
+    Plug.Adapters.Cowboy.child_spec(
+      scheme: :http,
+      options: [port: 5454],
+      plug: __MODULE__
+    )
+  end
+end
