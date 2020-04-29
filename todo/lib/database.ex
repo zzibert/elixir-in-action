d5ad202cb976476942c0769ad8cef30b30b98f54
zan <zan_zibert@hotmail.com>
2020-04-29 19:35:35 +0200

    storing data on all nodes

--- todo/lib/database.ex
+++ todo/lib/database.ex
@@ -18,13 +18,26 @@ defmodule Todo.Database do
     )
   end
 
-  def store(key, data) do
+  def store_local(key, data) do
     :poolboy.transaction(
       __MODULE__,
       fn worker_pid ->
         Todo.DatabaseWorker.store(worker_pid, key, data)
       end
     )
+  end
+
+  def store(key, data) do
+    {_results, bad_nodes} =
+      :rpc.multicall(
+        __MODULE__,
+        :store_local,
+        [key, data],
+        :timer.seconds(5)
+      )
+
+    Enum.each(bad_nodes, &IO.puts("Store failed on node #{&1}"))
+    :ok
   end
 
   def get(key) do
