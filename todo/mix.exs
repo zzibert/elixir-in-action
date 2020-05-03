3cfbd3fc61e85e41f5660be882feb2eec7860240
zan <zan_zibert@hotmail.com>
2020-05-03 11:41:06 +0200

    enforcing prod environment for the release task

--- todo/mix.exs
+++ todo/mix.exs
@@ -7,7 +7,8 @@ defmodule Todo.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [release: :prod]
     ]
   end
 
